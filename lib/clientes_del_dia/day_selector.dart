c52194905e5244bf478175ac1ddfbb55a94ca759
ImDaniela <danielavlealr@gmail.com>
2023-05-20 13:38:12 +0200

    Cambios: 1. Creado selector de dias de lunes a sabado 2. Creada lista de clientes

---
+++ lib/clientes_del_dia/day_selector.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class DaySelector extends StatefulWidget {
+  final Function(int)? onChanged;
+  const DaySelector({this.onChanged, Key? key}) : super(key: key);
+
+  @override
+  State<DaySelector> createState() => _DaySelectorState();
+}
+
+class _DaySelectorState extends State<DaySelector> {
+  int selected_index = 1;
+  void ChangeIndex(int cambio) {
+    if (widget.onChanged != null) widget.onChanged!(cambio);
+    setState(() {
+      selected_index = cambio;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color.fromRGBO(142, 11, 44, 1),
+        padding: EdgeInsets.only(top: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Lu',
+                index: 1,
+                selected_index: selected_index),
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Ma',
+                index: 2,
+                selected_index: selected_index),
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Mi',
+                index: 3,
+                selected_index: selected_index),
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Ju',
+                index: 4,
+                selected_index: selected_index),
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Vi',
+                index: 5,
+                selected_index: selected_index),
+            _customButton(
+                onPressed: ChangeIndex,
+                texto: 'Sa',
+                index: 6,
+                selected_index: selected_index),
+          ],
+        ));
+  }
+
+  Widget _customButton(
+      {Function? onPressed,
+      String? texto,
+      required int index,
+      required int selected_index}) {
+    const double size = 40;
+    return Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+            color: index == selected_index ? Colors.grey : null,
+            borderRadius: BorderRadius.circular(80)),
+        child: InkWell(
+          onTap: () {
+            if (onPressed != null) onPressed(index);
+          },
+          child: Center(
+              child: Text(
+            texto ?? '',
+            style: const TextStyle(color: Colors.white),
+          )),
+        ));
+  }
+}
--- lib/clientes_del_dia/list_screen.dart
+++ lib/clientes_del_dia/list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bys_app/clientes_del_dia/day_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:bys_app/componentes_comunes/navigation_bar.dart';
 
@@ -26,21 +27,22 @@ class _ListScreen extends State<ListScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Column(
           children: <Widget>[
             Container(
-              padding: const EdgeInsets.all(10),
+              padding: const EdgeInsets.only(top: 60, bottom: 40),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
+                    padding: EdgeInsets.only(bottom: 10),
                     alignment: Alignment.center,
                     child: const Text(
                       'Relación Cliente', // Add your label text here
                       textAlign: TextAlign.right,
                       style:
-                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Container(
@@ -54,7 +56,9 @@ class _ListScreen extends State<ListScreen> {
                               borderRadius: BorderRadius.circular(50.0),
                             ),
                             prefixIcon: const Icon(
-                                Icons.search), // Ícono antes del texto
+                              Icons.search,
+                              color: Colors.grey,
+                            ), // Ícono antes del texto
                             filled:
                                 true, // Enable filling the TextField background
                             fillColor: const Color.fromRGBO(
@@ -68,8 +72,104 @@ class _ListScreen extends State<ListScreen> {
                           ))),
                 ],
               ),
-            )
+            ),
+            DaySelector(
+              onChanged: (index) {
+                print(index);
+              },
+            ),
+            Expanded(
+                child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: DataTable(
+                          headingRowColor:
+                              MaterialStateProperty.resolveWith<Color>(
+                                  (states) =>
+                                      const Color.fromRGBO(142, 11, 44, 1)),
+                          columns: const <DataColumn>[
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Código',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Nombre comercial',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Ord',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Am-Pm',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Rep',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Nombre fiscal',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ],
+                          rows: listado()),
+                    )))
           ],
         ));
+  }
+
+  List<DataRow> listado() {
+    List<DataRow> resultado = [];
+    for (int i = 0; i < 100; i++) {
+      resultado.add(const DataRow(
+        cells: <DataCell>[
+          DataCell(Text('Janine')),
+          DataCell(Text('43')),
+          DataCell(Text('Professor')),
+          DataCell(Text('Sarah')),
+          DataCell(Text('19')),
+          DataCell(Text('Student'))
+        ],
+      ));
+    }
+    return resultado;
   }
 }
